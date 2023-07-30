0bac7fef6965d907e531a63a9d079f0118df1310
Pedro <pferezin@magnasistemas.com.br>
2023-07-29 21:44:18 -0300

    Initial Commit

---
+++ iQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iQuiz
+//
+//  Created by Pedro Henrique on 29/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
